9fe922c653b50b0e3083c5fb2d0dc59f760fdad7
wisdomstar94 <wisdomstar94@gmail.com>
2024-04-30 08:37:24 +0900

    fix

--- ios/RTNCalculator.mm
+++ ios/RTNCalculator.mm
@@ -13,7 +13,7 @@ RCT_EXPORT_MODULE()
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativeCalculatorSpecJSI>(params);
+    return std::make_shared<facebook::react::NativeRtnCalculatorSpecJSI>(params);
 }
 #endif
 
